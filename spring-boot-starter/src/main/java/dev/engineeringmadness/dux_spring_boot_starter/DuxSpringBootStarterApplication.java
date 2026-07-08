c38a6924a450ec9c902eedd84afbbd692d8d4346
Karan Gupta <gkaran184@gmail.com>
2026-07-08 19:59:51 +0530

    add spring boot starter

---
+++ spring-boot-starter/src/main/java/dev/engineeringmadness/dux_spring_boot_starter/DuxSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package dev.engineeringmadness.dux_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuxSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuxSpringBootStarterApplication.class, args);
+	}
+
+}
